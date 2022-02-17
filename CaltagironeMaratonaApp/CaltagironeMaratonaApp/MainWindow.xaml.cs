85f05525ef9be1d1a7c3fddbf3a360d4b0d2d2d4
EnniCaltagirone <ennixenni@gmail.com>
2022-02-17 13:30:07 +0100

    Prima consegna fatta

---
+++ CaltagironeMaratonaApp/CaltagironeMaratonaApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaltagironeMaratonaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ElencoMaratone elenco;
+        public MainWindow()
+        {
+            InitializeComponent();
+            elenco = new ElencoMaratone();
+            DgElencoMaratone.ItemsSource = elenco.Elenco;
+        }
+        private void BtnLeggiFile_Click(object sender, RoutedEventArgs e)
+        {
+            elenco.LeggiDaFile();
+            DgElencoMaratone.Items.Refresh();
+        }
+
+        private void BtnTempo_Click(object sender, RoutedEventArgs e)
+        {
+            string Atleta = TxtAtleta.Text;
+            string Città = TxtCittà.Text;
+            string TempoTrovato = elenco.CercaAtleta(Atleta,Città);
+            LblTempo.Content = TempoTrovato;
+        }
+    }
+}
---
+++ CaltagironeMaratonaApp/CaltagironeMaratonaApp/Maratona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaltagironeMaratonaApp
+{
+    class Maratona
+    {
+        public string Nome { get; set; }
+        public string Società { get; set; }
+        public int TempoInMinuti { get; set; }
+        public string Città { get; set; }
+
+    }
+}
